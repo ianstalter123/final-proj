6a77ff5470841dc917a108d6759d405270aaf858
ianstalter123 <istalter66@gmail.com>
2015-09-23 16:37:16 -0700

    updated mailer settings host for heroku

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "istalter66@gmail.com"
-  default_url_options[:host] = "localhost:9292"
+  default_url_options[:host] = "https://limitless-ocean-8627.herokuapp.com/"
+  # default_url_options[:host] = "localhost:9292"
 end
