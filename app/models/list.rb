89ce957b6f624641cf1db4eb6ed20125f1848027
ianstalter123 <istalter66@gmail.com>
2015-09-28 15:39:34 -0700

    trying to fix issue"

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,9 +1,6 @@
 class List < ActiveRecord::Base
 	has_many :lprices, dependent: :destroy
-  validates :title, presence: true
-  validates :url, presence: true
-  validates :price, presence: true
-  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+ 
 
 	  def self.get_am(asin)
 		req = Vacuum.new
